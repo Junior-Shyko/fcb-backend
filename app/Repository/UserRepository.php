55a9c2fc3477e0436c6e5ff54de071be1928701f
JuinEsp <junior.oliveira@esp.ce.gov.br>
2023-01-16 11:12:57 -0300

    Criado metodo getGroups

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -11,16 +11,32 @@ class UserRepository {
 	{
 		return  User::leftJoin('group_users', 'users.id' , '=', 'group_users.user_id')
 					->leftJoin('links', 'users.link_id', '=', 'links.id')
-					->leftJoin('groups', 'group_users.group_id', '=', 'groups.id')
-					->where('users.active','=', 1)						
+					->leftJoin('groups', 'group_users.group_id', '=', 'groups.id')					
 					->select(
-						'users.id as idUser', 'users.name as nameUser', 
+						'users.id as idUser', 'users.name as nameUser', 'users.active', 
 						'links.id as idLink', 'links.name as nameLink',
 						'groups.name as nameGroup', 'groups.id as idGroup'
 					)
+					// ->limit(1)
 					->get();
 		
 	}
+
+	public function getGroups($id)
+	{
+		return  User::leftJoin('group_users', 'users.id' , '=', 'group_users.user_id')
+					->leftJoin('links', 'users.link_id', '=', 'links.id')
+					->leftJoin('groups', 'group_users.group_id', '=', 'groups.id')					
+					->select(
+						'users.*', 
+						'group_users.id as idGroupUser','group_users.user_id', 'group_users.group_id',
+						'links.id as idLink', 'links.name as nameLink',
+						'groups.name as nameGroup', 'groups.id as idGroup'
+					)
+					->where('users.id', '=', $id)
+					->get();
+	}
+
 }
 
 
